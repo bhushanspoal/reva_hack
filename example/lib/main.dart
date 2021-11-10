ac163c00158a201712f4c9ffb49d282008db660e
bhushanspoal <bhushanbarca@gmail.com>
2021-11-10 18:49:17 +0530

    setting up the AR Core plugin

---
+++ example/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'app.dart';
+import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart'
+    show ArCoreController;
+
+void main() async
+{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  print('ARCORE IS AVAILABLE?');
+  print(await ArCoreController.checkArCoreAvailability());   //checking if the device supports the ar core
+
+  print('\nAR SERVICES INSTALLED?');
+  print(await ArCoreController.checkIsArCoreInstalled());
+
+  runApp(
+      App(),
+  );
+}
+
+
+
